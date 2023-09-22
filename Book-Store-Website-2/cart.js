07f2abd7207082c547d51cdf1324d587dbc0a9de
Kirubel <kirubeldess@gmail.com>
2023-09-22 21:55:14 +0300

    total bill and clear cart

--- Book-Store-Website-2/cart.js
+++ Book-Store-Website-2/cart.js
@@ -98,6 +98,7 @@ let update = (id)=>{
     let search = basket.find((x)=> x.id === id);
     document.getElementById(id).innerHTML = search.item;
     calculation();
+    totalAmount();
 };
 
 let removeItem = (id)=>{
@@ -105,6 +106,31 @@ let removeItem = (id)=>{
     console.log(selectedItem.id);
     basket = basket.filter((x)=>x.id !== selectedItem.id);
     generateCartItems();
+    totalAmount();
     localStorage.setItem("data",JSON.stringify(basket));
-    
 }
+
+let clearCart = ()=>{
+    basket = []
+    generateCartItems();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+}
+
+let totalAmount = ()=>{
+    if (basket.length !==0){
+        let amount = basket.map((x)=>{
+            let {item,id} = x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+
+            return item * search.price;
+        }).reduce((x,y)=>x + y ,0)
+        console.log(amount);
+        label.innerHTML  = `
+            <h2>Total Bill : ${amount} Birr</h2>
+            <button class="checkout">Checkout</button>
+            <button onclick="clearCart()" class= "removeAll">Clear Cart</button>
+        `
+    }else return;
+}
+totalAmount();
